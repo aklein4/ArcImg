af60e814442a48d3065800a8cd2877b287fa6fc4
Adam Klein <adam3207@gmail.com>
2024-05-15 03:16:14 -0700

    remove probes

--- src/models/arc_vit.py
+++ src/models/arc_vit.py
@@ -111,16 +111,13 @@ class ArcVitForImageClassification(ViTPreTrainedModel):
         # get logits for all versions
         hidden_states = self.vit(pixel_values)
         class_logits = self.classifier(hidden_states)
-        log_print("CLASS LOGITS!")
 
         true_states = self.vit(pixel_values, labels=true_labels)
         true_logits = self.classifier(true_states)
-        log_print("TRUE LOGITS!")
 
         fake_labels = torch.distributions.Categorical(logits=class_logits).sample()
         fake_states = self.vit(pixel_values, labels=fake_labels)
         fake_logits = self.classifier(fake_states)
-        log_print("FAKE LOGITS!")
 
         # get arc outputs
         ar = torch.arange(bs, device=class_logits.device, dtype=torch.long)
@@ -130,12 +127,10 @@ class ArcVitForImageClassification(ViTPreTrainedModel):
 
         true_arc = tmp_true_logits[ar, true_labels] - tmp_class_logits[ar, true_labels].detach()
         fake_arc = tmp_fake_logits[ar, fake_labels] - tmp_class_logits[ar, fake_labels].detach()
-        log_print("ARCS!")
 
         # normalize class logits
         if not debug:
             class_logits = F.log_softmax(class_logits, dim=-1)
-        log_print("LOG SOFTMAX!")
 
         return (
             class_logits,
